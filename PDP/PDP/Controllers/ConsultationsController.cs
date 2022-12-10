cfb7c9c2ba9ebe14904f27105d82e128096ed9a9
ReksioCroft <octavian.staicu@s.unibuc.ro>
2022-12-10 18:16:18 +0200

    wip

---
+++ PDP/PDP/Controllers/ConsultationsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PDP.Models;
+
+namespace PDP.Controllers
+{
+    public class ConsultationsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Specializations
+        [Authorize(Roles = "Admin")]
+        public ActionResult Index()
+        {
+            return View(db.Specializations.ToList());
+        }
+
+    }
+}
