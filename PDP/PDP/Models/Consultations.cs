dba7bfd5957d977e538b685d42a938a91542388e
ReksioCroft <octavian.staicu@s.unibuc.ro>
2022-12-10 16:35:01 +0200

    create consultations model

---
+++ PDP/PDP/Models/Consultations.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity.Core.Common.CommandTrees;
+using System.Linq;
+using System.Web;
+
+namespace PDP.Models
+{
+    public class Consultation
+    {
+        [Key]
+        public int ConsultationID { get; set; }
+
+        [Required(ErrorMessage = "Please pick a start date!")]
+        DateTime startDate { get; set; }
+
+        [Required(ErrorMessage = "Please pick an end date!")]
+        DateTime endDate { get; set; }
+
+        public double price { get; set; }
+        public bool canceled { get; set; }
+
+        public virtual Doctor Doctor { get; set; }
+        public virtual ApplicationUser user { get; set; }
+
+    }
+}
